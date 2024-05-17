5f3f8b992c2fe2d20addb71dd1fa7ef98d8de274
Stanley Kudrow <stankudrow@reply.no>
2024-05-18 00:48:42 +0300

    add the Job (pydantic) model

--- sprint2/job.py
+++ sprint2/job.py
@@ -1,8 +1,31 @@
-class Job:
-    pass
+from datetime import datetime
+from typing import Any, Callable
+
+from pydantic import BaseModel, NonNegativeInt
 
 
-import asyncio
-import asyncio.coroutines
+class Job(BaseModel):
+    fn: Callable
+    args: tuple[Any, ...] = ()
+    kwargs: dict[str, Any] = {}
+    max_retries: NonNegativeInt = 0
+    start: None | datetime = None
+    duration: None | NonNegativeInt = None
+    dependencies: tuple["Job", ...] = ()
 
-asyncio.coroutines.coroutine
+    def __lt__(self, other: "Job") -> bool:
+        if self.start is None:
+            return True
+        if other.start is None:
+            return False
+        return self.start < other.start
+
+    # redundant, but OK
+    def to_dict(self) -> dict[str, Any]:
+        """Returns the job as a dictionary.
+
+        Returns:
+            dict[str, Any]
+        """
+
+        return self.model_dump()
---
+++ tests/test_job.py
@@ -0,0 +1,162 @@
+from datetime import datetime, timedelta
+from typing import Any
+
+import pytest
+
+from sprint2.job import Job
+
+
+NOW = datetime.now()
+FUTURE = NOW + timedelta(microseconds=1)
+PAST = NOW - timedelta(microseconds=1)
+
+
+def _fn():
+    return None
+
+
+class _Functor:
+    def __call__(self, *args, **kwargs):
+        return len(args) + len(kwargs)
+
+
+@pytest.mark.parametrize(
+    ("job1", "job2", "answer"),
+    [
+        (
+            Job(fn=_fn),
+            Job(fn=_fn),
+            True,
+        ),
+        (
+            Job(fn=_fn, start=None),
+            Job(fn=_fn, start=NOW),
+            True,
+        ),
+        (
+            Job(fn=_fn, start=NOW),
+            Job(fn=_fn, start=None),
+            False,
+        ),
+        (
+            Job(fn=_fn, start=NOW),
+            Job(fn=_fn, start=NOW),
+            False,
+        ),
+        (
+            Job(fn=_fn, start=PAST),
+            Job(fn=_fn, start=NOW),
+            True,
+        ),
+        (
+            Job(fn=_fn, start=FUTURE),
+            Job(fn=_fn, start=NOW),
+            False,
+        ),
+    ],
+)
+def test_job_less_than(job1: Job, job2: Job, answer: bool) -> None:
+    res = job1 < job2
+
+    assert res == answer
+
+
+@pytest.mark.parametrize(
+    ("job1", "job2", "answer"),
+    [
+        (
+            Job(fn=_fn),
+            Job(fn=_fn),
+            True,
+        ),
+        (
+            Job(fn=_fn, start=None),
+            Job(fn=_fn, start=NOW),
+            False,
+        ),
+        (
+            Job(fn=_fn, start=NOW),
+            Job(fn=_fn, start=None),
+            False,
+        ),
+        (
+            Job(fn=_fn, start=NOW),
+            Job(fn=_fn, start=NOW),
+            True,
+        ),
+        (
+            Job(fn=_fn, start=PAST),
+            Job(fn=_fn, start=NOW),
+            False,
+        ),
+        (
+            Job(fn=_fn, start=FUTURE),
+            Job(fn=_fn, start=NOW),
+            False,
+        ),
+    ],
+)
+def test_job_equality(job1: Job, job2: Job, answer: bool) -> None:
+    res = job1 == job2
+
+    assert res == answer
+
+
+@pytest.mark.parametrize(
+    ("job", "answer"),
+    [
+        (
+            Job(fn=_fn),
+            {
+                "fn": _fn,
+                "args": (),
+                "kwargs": {},
+                "max_retries": 0,
+                "start": None,
+                "duration": None,
+                "dependencies": (),
+            },
+        ),
+        (
+            Job(
+                fn=_fn,
+                args=[1, "2"],
+                kwargs={"a": 7, "b": [4]},
+                max_retries=2,
+                start=PAST,
+                duration=1,
+                dependencies=[
+                    Job(
+                        fn=_Functor,
+                        args={6},
+                        kwargs={"lol": "kek"},
+                        duration=5,
+                    )
+                ],
+            ),
+            {
+                "fn": _fn,
+                "args": (1, "2"),
+                "kwargs": {"a": 7, "b": [4]},
+                "max_retries": 2,
+                "start": PAST,
+                "duration": 1,
+                "dependencies": (
+                    {
+                        "fn": _Functor,
+                        "args": (6,),
+                        "kwargs": {"lol": "kek"},
+                        "max_retries": 0,
+                        "start": None,
+                        "duration": 5,
+                        "dependencies": (),
+                    },
+                ),
+            },
+        ),
+    ],
+)
+def test_to_dict(job: Job, answer: dict[str, Any]) -> None:
+    dct = job.to_dict()
+
+    assert dct == answer
